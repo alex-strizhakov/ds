6a3d400cfb66eb6f5d739fa1e25984deb7979cd2
Alexander <alex.strizhakov@gmail.com>
2019-12-08 14:13:38 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DS.MixProject do
   def project do
     [
       app: :ds,
-      version: "1.0.0",
+      version: "1.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
