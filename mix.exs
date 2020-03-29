4b7abcea4d72576dd8139c7be744f2cb5a164d3d
Alexander Strizhakov <alex.strizhakov@gmail.com>
2020-03-29 16:36:21 +0300

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DS.MixProject do
   def project do
     [
       app: :ds,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -29,7 +29,7 @@ defmodule DS.MixProject do
   defp deps do
     [
       {:plug_cowboy, "~> 2.1"},
-      {:ua_inspector, "~> 1.0"},
+      {:ua_inspector, "~> 1.2"},
       {:jason, "~> 1.1"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
