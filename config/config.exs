d1279a89f449cd9b6a0475c92ed5e7f2ca2727b1
Alexander Strizhakov <alex.strizhakov@gmail.com>
2020-04-05 16:06:59 +0300

    don't use config

--- config/config.exs
+++ config/config.exs
@@ -28,6 +28,3 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
-config :ua_inspector,
-  database_path: "ua_inspector",
-  pool: [max_overflow: 100, size: 1000]
